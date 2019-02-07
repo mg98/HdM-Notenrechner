3ce590d3d5aa4b9efa64a795ba6293100e1cddaf
Marcel Gregoriadis <marcel.g98@live.de>
2019-02-07 21:42:14 +0100

    fixed wrong import path, added npm run prod

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const request = require('request')
 const cheerio = require('cheerio')
 const url = require('url')
-const hdmModule = require('../hdm-module')
+const hdmModule = require('../storage/hdm-module')
 
 const port = 8080
 
