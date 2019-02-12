a56d93d672a36c22d1141faccd7b13920e5d3b90
Marcel Gregoriadis <marcel.g98@live.de>
2019-02-12 23:21:15 +0100

    imported all bachelor courses

--- server/src/import.js
+++ server/src/import.js
@@ -6,14 +6,19 @@ const hdmModule = []
 
 fs.readdir(PATH, function(err, files) {
     for (let i = 0; i < files.length; i++) {
-        const body = fs.readFileSync(PATH + files[i], 'utf8');
+        const fileParts = files[i].split('_')
+        const studies = fileParts[0]
+        const sort = fileParts[1].substring(0, fileParts[1].length - '.html'.length)
 
+        const body = fs.readFileSync(PATH + files[i], 'utf8');
         const $ = cheerio.load(body)
         const tableRows = $('table tr:not(:first-child, :nth-child(2), :nth-child(3))')
 
         tableRows.each(function() {
             const hdmModul = {
                 edvNr: $(this).find('td').eq(0).html().trim(),
+                studies: studies,
+                sort: sort,
                 name: $(this).find('td').eq(1).html().trim(),
                 ects: parseInt($(this).find('td').eq(2).html().trim())
             }
